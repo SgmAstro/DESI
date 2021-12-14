9ff889f5bd5d39d35866d5e4e63ca0dfe2f52a9a
Sam Moore <sgmoore@princeton.edu>
2021-12-14 04:00:08 -0800

    add tmr_q

--- tmr_ecorr.py
+++ tmr_ecorr.py
@@ -8,3 +8,17 @@ def tmr_ecorr(redshift, restframe_colour, all=False):
         result[restframe_colour <= 0.62] = -Q_BLUE*redshift[restframe_colour <= 0.62]
 
     return result
+
+
+def tmr_q(redshift, restframe_colour, all=False):
+    if all:
+        #result = -Q_ALL*redshift
+        result = np.ones_like(redshift) * Q_ALL
+        return result
+    
+    else:
+        result = np.zeros_like(redshift)
+        result[restframe_colour > 0.62] = Q_RED
+        result[restframe_colour <= 0.62] = Q_BLUE
+    
+    return result

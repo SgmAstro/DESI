a77cb67ba6afa7c4e212590ea812ced66dc24df1
michaelJwilson <mjwilson@lbl.gov>
2021-12-16 05:15:51 -0800

    Tidy file structure.

--- cosmo.py
+++ cosmo.py
@@ -1,12 +1,14 @@
 import numpy as np
 import astropy.units as u
 
-from   fsky import fsky
 from   astropy.cosmology       import  FlatLambdaCDM
 
 # setting cosmological parameters
 h = 1
 cosmo = FlatLambdaCDM(H0=100*h * u.km / u.s / u.Mpc, Tcmb0=2.725 * u.K, Om0= 0.25)
+
+def fsky(area_sqdeg):
+    return area_sqdeg / 41252.96
 
 def distmod(zs):
     return 5. * np.log10(cosmo.luminosity_distance(zs).value) + 25.
--- fsky.py
+++
@@ -1,2 +0,0 @@
-def fsky(area_sqdeg):
-    return area_sqdeg / 41252.96

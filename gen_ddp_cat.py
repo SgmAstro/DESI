25698f702a30b195f39c45b8cfd44708947ec205
michaelJwilson <mjwilson@lbl.gov>
2021-12-17 01:43:48 -0800

    ddp tidy.

--- gen_ddp_cat.py
+++ gen_ddp_cat.py
@@ -8,6 +8,11 @@ fpath = os.environ['CSCRATCH'] + '/norberg/GAMA4/gama_gold_zmax.fits'
 dat   = fitsio.read(fpath)
 dat   = Table(dat)
 
-dat['DDP'] = get_ddps(dat['MCOLOR_0P0'], dat['ZGAMA'])
+Area  = 180. 
+
+dat['DDP'], zlims = get_ddps(Area, dat['MCOLOR_0P0'], dat['ZGAMA'])
+dat.meta = zlims
+
+print(zlims)
 
 dat.write(fpath.replace('zmax', 'ddp'), format='fits', overwrite=True)

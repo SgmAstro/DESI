bdede7e489572bce71cf5c8dccdffe5b48bc7df8
michaelJwilson <mjwilson@lbl.gov>
2021-12-17 09:05:55 -0800

    tidy.

--- is_gfield.py
+++
@@ -1,8 +0,0 @@
-from gama_limits import gama_limits
-
-def is_gfield(ra, dec, gfield):
-    
-    if (ra > gama_limits[gfield]['ra_min']) & (ra < gama_limits[gfield]['ra_max']) & (dec > gama_limits[gfield]['dec_min']) & (dec < gama_limits[gfield]['dec_max']):
-        return 1
-    else:
-        return 0

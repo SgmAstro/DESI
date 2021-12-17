6c31a672c71a2aa082df3e6e9b11928709b2d0ef
michaelJwilson <mjwilson@lbl.gov>
2021-12-16 17:28:40 -0800

    Tidy ddps.

--- ddp.py
+++ ddp.py
@@ -2,38 +2,38 @@ import os
 import fitsio
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import interpolate
 
-def ddp(M_0P0, z):
-    tmr_DDP1Mr = [-20.1, -21.8]
-    tmr_DDP2Mr = [-19.3, -20.6]
-    tmr_DDP3Mr = [-17.8, -19.6]
+from   scipy.interpolate import interp1d
 
-    gmr_0P1, gmr_0P0 = 0.158, 0.16
-    fpath = os.environ['CSCRATCH'] + '/norberg/GAMA4/ddrp_limits/ddrp_limit_{}_{}.fits'.format(gmr_0P1, gmr_0P0)
-    curve2 = fitsio.read(fpath)
 
-    gmr_0P1, gmr_0P0 = 0.96, 0.96
-    fpath = os.environ['CSCRATCH'] + '/norberg/GAMA4/ddrp_limits/ddrp_limit_{}_{}.fits'.format(gmr_0P1, gmr_0P0)
-    curve1 = fitsio.read(fpath)
+tmr_DDP1     = [-21.8, -20.1]
+tmr_DDP2     = [-20.6, -19.3]
+tmr_DDP3     = [-19.6, -17.8]
 
-    f1 = interpolate.interp1d(curve1['MR_0P0'], curve1['Z'], kind='linear', copy=True, bounds_error=True, fill_value=np.NaN, assume_sorted=False)
+root         = os.environ['CSCRATCH'] + '/norberg/GAMA4/ddrp_limits/'
 
-    f2 = interpolate.interp1d(curve2['MR_0P0'], curve2['Z'], kind='linear', copy=True, bounds_error=True, fill_value=np.NaN, assume_sorted=False)
+bright_curve = fitsio.read(root + '/ddrp_limit_7.fits')
+faint_curve  = fitsio.read(root + '/ddrp_limit_27.fits')
 
-    Mr_new1 = np.arange(curve1['MR_0P0'].min(), curve1['MR_0P0'].max(), 0.5)
-    Mr_new2 = np.arange(curve2['MR_0P0'].min(), curve2['MR_0P0'].max(), 0.5)
+# TODO: extend the curve limits and put bounds_error back on.
+bright_curve = interp1d(bright_curve['M0P0_QCOLOR'], bright_curve['Z'], kind='linear', copy=True, bounds_error=False, fill_value=0.0, assume_sorted=False)
+faint_curve  = interp1d(faint_curve['M0P0_QCOLOR'], faint_curve['Z'], kind='linear', copy=True, bounds_error=False, fill_value=1.0, assume_sorted=False)
 
-    tmr_DDP1z = [f1(tmr_DDP1Mr), f2(tmr_DDP1Mr)]
-    tmr_DDP2z = [f1(tmr_DDP2Mr), f2(tmr_DDP2Mr)]
-    tmr_DDP3z = [f1(tmr_DDP3Mr), f2(tmr_DDP3Mr)]
+def get_ddps(M_0P0s, zs):
+    result   = np.zeros(len(zs) * 3, dtype=int).reshape(len(zs), 3)
+    
+    exclude  = (zs > faint_curve(M_0P0s)) | (zs < bright_curve(M_0P0s))
 
-    DDP1_true = (M_0P0 < tmr_DDP1Mr[0]) & (M_0P0 > tmr_DDP1Mr[1]) & (z < tmr_DDP1z[0][0]) & (z > tmr_DDP1z[1][1])
-    DDP2_true = (M_0P0 < tmr_DDP2Mr[0]) & (M_0P0 > tmr_DDP2Mr[1]) & (z < tmr_DDP2z[0][0]) & (z > tmr_DDP2z[1][1])
-    DDP3_true = (M_0P0 < tmr_DDP3Mr[0]) & (M_0P0 > tmr_DDP3Mr[1]) & (z < tmr_DDP3z[0][0]) & (z > tmr_DDP3z[1][1])
+    for i, lims in enumerate([tmr_DDP1, tmr_DDP2, tmr_DDP3]):
+        in_ddp = (M_0P0s >= lims[0]) & (M_0P0s <= lims[1])
+        
+        result[in_ddp, i] = 1
 
+    result[exclude] = 0
+        
     # returns [0, 1, 0] array
-    # need to covert bool to int
-    return [DDP1_true, DDP2_true, DDP3_true]
+    return  result
 
 
+if __name__ == '__main__':
+    print('Done.')
--- gen_ddp_cat.py
+++ gen_ddp_cat.py
@@ -1,16 +1,13 @@
 import os
 import fitsio
 from astropy.table import Table
-from ddp import ddp
+from ddp import get_ddps
 
-fpath = os.environ['CSCRATCH'] + '/norberg/GAMA4/gama_gold_kE_20k.fits'
-dat = fitsio.read(fpath)
-dat = Table(dat)
 
-ddp_array = ddp(dat['MCOLOR_0P0'], dat['ZGAMA'])
+fpath = os.environ['CSCRATCH'] + '/norberg/GAMA4/gama_gold_zmax.fits'
+dat   = fitsio.read(fpath)
+dat   = Table(dat)
 
-dat['DDP1'] = ddp_array[0]
-dat['DDP2'] = ddp_array[1]
-dat['DDP3'] = ddp_array[2]
+dat['DDP'] = get_ddps(dat['MCOLOR_0P0'], dat['ZGAMA'])
 
-dat.write(fpath.replace('20k', '20k_ddp'), format='fits', overwrite=True)
+dat.write(fpath.replace('zmax', 'ddp'), format='fits', overwrite=True)

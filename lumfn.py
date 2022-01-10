88a284fe60d36fa411bce8c41832a463a2ddf571
michaelJwilson <mjwilson@lbl.gov>
2022-01-10 10:15:18 -0800

    Correct LF errors - Condon 1988.

--- lumfn.py
+++ lumfn.py
@@ -14,15 +14,27 @@ def lumfn(dat, vol, Ms=np.arange(-25.5, -15.5, 0.3), Mcol='MCOLOR_0P0'):
     assert np.all(ds == dM)
     
     for idx in np.unique(idxs):
-        sample = dat[idxs == idx]
+        sample  = dat[idxs == idx]
         nsample = len(sample)
         
-        median = np.median(sample[Mcol])
+        median  = np.median(sample[Mcol])
 
-        ivmax = 1./sample['VMAX'].data
-        variance = np.sum(ivmax**2.) / np.sum(ivmax)**2.
+        ivmax   = 1. / sample['VMAX'].data
+        ivmax2  = 1. / sample['VMAX'].data**2.
         
-        result.append([median, nsample / dM / vol, np.sqrt(nsample) / dM / vol, np.sum(ivmax) / dM, np.sqrt(variance) / dM / vol])
+        result.append([median,\
+                       nsample / dM / vol,\
+                       np.sqrt(nsample) / dM / vol,\
+                       np.sum(ivmax) / dM,\
+                       np.sqrt(np.sum(ivmax2)) / dM,\
+                       nsample,
+                       np.median(sample['VMAX'].data) / vol])
 
+    names = ['MEDIAN_M', 'PHI_N', 'PHI_N_ERROR', 'PHI_IVMAX', 'PHI_IVMAX_ERROR', 'N', 'V_ON_VMAX']
+        
+    result = Table(np.array(result), names=names)
+
+    result.meta['VOLUME'] = vol
+    
     # TODO: PHI_IVMAX_ERROR is not right.
-    return  Table(np.array(result), names=['MEDIAN_M', 'PHI_N', 'PHI_N_ERROR', 'PHI_IVMAX', 'PHI_IVMAX_ERROR'])
+    return  result

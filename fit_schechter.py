d90fb2ed0097302411031490d4169b820175cacd
Sam Moore <37001791+SgmAstro@users.noreply.github.com>
2022-01-11 19:57:44 +0000

    Add files via upload

--- fit_schechter.py
+++ fit_schechter.py
@@ -21,6 +21,8 @@ lumfn = Table.read(fpath)
 if known:
     lumfn['PHI_N'] = schechter(lumfn['MEDIAN_M'], -2.01, -20.89, -1.25)
     lumfn['PHI_N_ERROR'] = 1.e-2 * lumfn['PHI_N']
+else:
+    lumfn = lumfn[lumfn['PHI_N'] > 0.]
 
 lumfn.pprint()
 
@@ -32,29 +34,26 @@ def chi2(log10phistar, Mstar, alpha):
     res  = res * res
 
     return np.sum(res)
-
+    
 def lnlike(log10phistar, Mstar, alpha):
     return -chi2(log10phistar, Mstar, alpha) / 2.
-    
+
+x2 = chi2(-2.01, -20.89, -1.25)
+print(x2)
+
 info = {"likelihood": {"schechter": lnlike}}
 
-# https://cobaya.readthedocs.io/en/latest/params_prior.html
 info["params"] = {
-     "log10phistar": {"prior": {"min": -2.5, "max": 0.0},   "ref": -2.0,   "proposal": 0.01},  # TMR ref.  -2.01
-     "Mstar":        {"prior": {"min": -21., "max": -20.5}, "ref": -20.75, "proposal": 0.01},  # TMR ref.  -20.89 
-     "alpha":        {"prior": {"min": -1.3, "max": -1.2},  "ref": -1.25,  "proposal": 0.01}}  # TMR ref.  -1.25
+     #"log10phistar": {"prior": {"min": -2.5, "max": 0.0},   "ref": -2.0,   "proposal": 0.01},  # TMR ref.  -2.01
+     #"Mstar":        {"prior": {"min": -21., "max": -20.5}, "ref": -20.75, "proposal": 0.01},  # TMR ref.  -20.89 
+     #"alpha":        {"prior": {"min": -1.3, "max": -1.2},  "ref": -1.25,  "proposal": 0.01}}  # TMR ref.  -1.25
 
-# https://cobaya.readthedocs.io/en/latest/sampler_mcmc.html
-info["sampler"] = {"mcmc": {"Rminus1_stop": 0.001, "max_tries": 1000}}
+     "log10phistar": {"prior": {"dist": "norm", "loc": -2.00, "scale": 0.25},   "ref": -2.00,   "proposal": 0.01},  # TMR ref.  -2.01
+     "Mstar":        {"prior": {"dist": "norm", "loc": -20.89, "scale": 0.15}, "ref": -20.89, "proposal": 0.01},  # TMR ref.  -20.89 
+     "alpha":        {"prior": {"dist": "norm", "loc": -1.25, "scale": 0.05},  "ref": -1.25,  "proposal": 0.01}}  # TMR ref.  -1.25
+
+info["sampler"] = {"mcmc": {"Rminus1_stop": 0.001, "max_tries": 5000}}
 
 updated_info, sampler = run(info, output='{}/cobaya/schechter_chain'.format(root))
 
 print('Written to {}/cobaya/schechter_chain*'.format(root))
-
-# gdsamples = MCSamplesFromCobaya(updated_info, sampler.products()["sample"])
-# gdplot    = gdplt.get_subplot_plotter(width_inch=5)
-
-# gdplot.triangle_plot(gdsamples, ["log10phistar", "Mstar", "alpha"], filled=True)
-
-# gdplot    = gdplt.get_subplot_plotter(width_inch=5)
-# gdplot.plots_1d(gdsamples, ["r", "theta"], nx=2)

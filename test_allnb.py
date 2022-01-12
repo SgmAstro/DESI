f1c8146994c9afbb70cd76914b21d5facc6e514d
michaelJwilson <mjwilson@lbl.gov>
2022-01-12 10:07:29 -0800

    Initial update of randoms qa.

--- test_allnb.py
+++ test_allnb.py
@@ -10,7 +10,7 @@ def test_answer():
 def test_notebooks():
     fields = ['G9', 'G12', 'G15']
     
-    for field in ['G9']:    
+    for field in fields:    
         pm.execute_notebook('docs/nb/randoms_qa.ipynb',\
                             'test/pm_randoms_qa.ipynb',\
                             parameters=dict(field=field)

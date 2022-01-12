4f951e79d3da171e949361cd6d7bc5092107e0bb
michaelJwilson <mjwilson@lbl.gov>
2022-01-12 09:25:55 -0800

    Updata IVMAX LF errors to Condon 1988.

--- lumfn.py
+++ lumfn.py
@@ -3,7 +3,7 @@ import numpy as np
 from   astropy.table import Table
 from   cosmo import volcom
 
-def lumfn(dat, vol, Ms=np.arange(-25.5, -15.5, 0.3), Mcol='MCOLOR_0P0'):
+def lumfn(dat, vol, Ms=np.arange(-25.5, -15.5, 0.1), Mcol='MCOLOR_0P0'):
     idxs = np.digitize(dat[Mcol], bins=Ms)
 
     result = []
@@ -13,16 +13,27 @@ def lumfn(dat, vol, Ms=np.arange(-25.5, -15.5, 0.3), Mcol='MCOLOR_0P0'):
 
     assert np.all(ds == dM)
     
-    for idx in np.unique(idxs):
+    for idx in np.arange(len(Ms) - 1):
         sample = dat[idxs == idx]
         nsample = len(sample)
         
         median = np.median(sample[Mcol])
 
         ivmax = 1./sample['VMAX'].data
-        variance = np.sum(ivmax**2.) / np.sum(ivmax)**2.
-        
-        result.append([median, nsample / dM / vol, np.sqrt(nsample) / dM / vol, np.sum(ivmax) / dM, np.sqrt(variance) / dM / vol])
+        ivmax2  = 1. / sample['VMAX'].data**2.
 
-    # TODO: PHI_IVMAX_ERROR is not right.
-    return  Table(np.array(result), names=['MEDIAN_M', 'PHI_N', 'PHI_N_ERROR', 'PHI_IVMAX', 'PHI_IVMAX_ERROR'])
+        result.append([median,\
+                       nsample / dM / vol,\
+                       np.sqrt(nsample) / dM / vol,\
+                       np.sum(ivmax) / dM,\
+                       np.sqrt(np.sum(ivmax2)) / dM,\
+                       nsample,
+                       np.median(sample['VMAX'].data) / vol])
+
+    names = ['MEDIAN_M', 'PHI_N', 'PHI_N_ERROR', 'PHI_IVMAX', 'PHI_IVMAX_ERROR', 'N', 'V_ON_VMAX']
+
+    result = Table(np.array(result), names=names)
+      
+    result.meta['VOLUME'] = vol
+
+    return  result 

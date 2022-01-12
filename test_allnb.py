ab511edaf5fe93bd805d607ce929d7d9c1e84e4b
michaelJwilson <mjwilson@lbl.gov>
2022-01-12 15:20:42 -0800

    all random test notebooks.

--- test_allnb.py
+++ test_allnb.py
@@ -1,12 +1,6 @@
 import papermill as pm
 
 # https://docs.pytest.org/en/6.2.x/
-def inc(x):
-    return x + 1
-
-def test_answer():
-    assert inc(4) == 5
-
 def test_notebooks():
     fields = ['G9', 'G12', 'G15']
     

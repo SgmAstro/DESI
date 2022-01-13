291ab27e837ffba8ce84eb56eaa0ed20d238b82b
Sam Moore <sgmoore@princeton.edu>
2022-01-13 08:40:05 -0800

    Renamed variables/arguments

--- renormalise_d8LF.py
+++ renormalise_d8LF.py
@@ -1,8 +1,13 @@
-def vol_normalise(cat, scale):
-    cat['PHI_N'] = cat['PHI_N']*scale
-    cat['PHI_N_ERROR'] = cat['PHI_N_ERROR']*scale
-    cat['PHI_IVMAX'] = cat['PHI_IVMAX']*scale
-    cat['PHI_IVMAX_ERROR'] = cat['PHI_IVMAX_ERROR']*scale
+def lumfn_d8_normalise(cat, fdelta):
+    
+    '''
+    fscale equal to Equation 7 in McNaught-Roberts (2014).
+    See: https://arxiv.org/pdf/1409.4681.pdf
+    '''
+    
+    cat['PHI_N'] = cat['PHI_N']*fdelta
+    cat['PHI_N_ERROR'] = cat['PHI_N_ERROR']*fdelta
+    cat['PHI_IVMAX'] = cat['PHI_IVMAX']*fdelta
+    cat['PHI_IVMAX_ERROR'] = cat['PHI_IVMAX_ERROR']*fdelta
     
     return cat
-    

9cd8a19a5c3d3c04816c8b997a275ab680f97d5e
michaelJwilson <mjwilson@lbl.gov>
2022-01-13 01:04:31 -0800

    Polish testing.

--- test_allnb.py
+++ test_allnb.py
@@ -1,10 +1,21 @@
 import papermill as pm
 
 # https://docs.pytest.org/en/6.2.x/
-def test_notebooks():
+def test_allnbs():
+    print('Running all tests.')
+    
+    run_randomqa()
+
+    print('Done.')
+    
+# TODO:  GitHub Action pytest calls fails due to randoms path not
+# existing. 
+def run_randomqa():
     fields = ['G9', 'G12', 'G15']
     
-    for field in fields:    
+    for field in fields:
+        print('Running random QA for field {}'.format(field))
+        
         pm.execute_notebook('docs/nb/randoms_qa.ipynb',\
                             'test/pm_randoms_qa_{}.ipynb'.format(field),\
                             parameters=dict(field=field)

ee7c72e8dc53973aa8249f10cd31d83f8abc7e46
michaelJwilson <mjwilson@lbl.gov>
2022-01-14 11:28:17 -0800

    d8 <= 4

--- delta8_limits.py
+++ delta8_limits.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-dd8_limits = [[-1.0, -0.75], [-0.4, 0.0], [0.7, 1.6], [4.0, 1.e6]]
+dd8_limits = [[-1.0, -0.75], [-0.4, 0.0], [0.7, 1.6], [-1.0, 4.]]
 
 def delta8_tier(delta8):
     result = -99 * np.ones(len(delta8), dtype=np.int)

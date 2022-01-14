01ab51a08e70c18a1d7fada47614ac5480c47bcb
michaelJwilson <mjwilson@lbl.gov>
2022-01-14 06:53:38 -0800

    Add delta8 qa pytest and sbatch.

--- test_allnb.py
+++ test_allnb.py
@@ -4,8 +4,10 @@ import papermill as pm
 def test_allnbs():
     print('Running all tests.')
     
-    run_randomqa()
+    # run_randomqa()
 
+    run_delta8qa()
+    
     print('Done.')
     
 # TODO:  GitHub Action pytest calls fails due to randoms path not
@@ -18,5 +20,16 @@ def run_randomqa():
         
         pm.execute_notebook('docs/nb/randoms_qa.ipynb',\
                             'test/pm_randoms_qa_{}.ipynb'.format(field),\
+                            parameters=dict(field=field)
+                            )
+
+def run_delta8qa():
+    fields = ['G9', 'G12', 'G15']
+
+    for field in fields:
+        print('Running delta8 QA for field {}'.format(field))
+
+        pm.execute_notebook('docs/nb/delta8_qa.ipynb',\
+                            'test/pm_delta8_qa_{}.ipynb'.format(field),\
                             parameters=dict(field=field)
                             )

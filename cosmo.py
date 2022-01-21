e14045f5d125371badb1af0dbbdf4fa6d1dc7b5a
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-01-21 09:55:15 +0000

    split main.

--- cosmo.py
+++ cosmo.py
@@ -4,11 +4,11 @@ import astropy.units as u
 from   astropy.cosmology       import  FlatLambdaCDM
 
 # setting cosmological parameters
-h = 1
+h     = 1
 cosmo = FlatLambdaCDM(H0=100*h * u.km / u.s / u.Mpc, Tcmb0=2.725 * u.K, Om0= 0.25)
 
 def fsky(area_sqdeg):
-    return area_sqdeg / 41252.96
+    return area_sqdeg / (360.**2. / np.pi)
 
 def distmod(zs):
     return 5. * np.log10(cosmo.luminosity_distance(zs).value) + 25.

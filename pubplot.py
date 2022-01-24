780698f85ca842bbd28f021f274a911b046e0224
Sam Moore <37001791+SgmAstro@users.noreply.github.com>
2022-01-24 11:27:14 +0000

    Added nice fonts (work in progress)

---
+++ pubplot.py
@@ -0,0 +1,14 @@
+'''
+Generate publication ready plots
+'''
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+nice_fonts = {
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif" : "Times New Roman",
+}
+matplotlib.rcParams.update(nice_fonts)
+

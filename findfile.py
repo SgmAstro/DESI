4c533f3f31ac95ec7cffb457e13184f753a01ea7
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-01-27 15:50:54 +0000

    Add findfile timestamp

--- findfile.py
+++ findfile.py
@@ -1,10 +1,13 @@
 import os
+import time
 import glob
+import datetime
 
 from   astropy.table import Table, vstack
+from   delta8_limits import d8_limits
 
 
-def findfile(ftype, dryrun=False, prefix='', field=None):
+def findfile(ftype, dryrun=False, prefix='', field=None, utier='{utier}'):
     if dryrun:
         dryrun = '_dryrun'
     else:
@@ -26,7 +29,7 @@ def findfile(ftype, dryrun=False, prefix='', field=None):
         fpath      = parts['dir'] + '{}_{}{}.fits'.format(parts['id'], parts['ftype'], dryrun)
 
     else:
-        file_types = {'ddp_n8_d0': {'dir': gold_dir, 'id': 'gama_gold', 'ftype': 'ddp_n8_d0'}}
+        file_types = {'ddp_n8_d0': {'dir': gold_dir, 'id': 'gama_gold', 'ftype': 'ddp_n8_d0_{}'.format(utier)}}
         parts      = file_types[ftype]
 
         fpath      = parts['dir'] + '{}_{}_{}{}.fits'.format(parts['id'], field, parts['ftype'], dryrun)
@@ -35,20 +38,32 @@ def findfile(ftype, dryrun=False, prefix='', field=None):
 
 
 if __name__ == '__main__':
+    fields = ['G9', 'G12', 'G15']
 
     fpaths = []
-    fpaths.append(findfile('gold',   dryrun=False, prefix='', field=None))
-    fpaths.append(findfile('kE',     dryrun=False, prefix='', field=None))
-    fpaths.append(findfile('zmax',   dryrun=False, prefix='', field=None))
-    fpaths.append(findfile('vmax',   dryrun=False, prefix='', field=None))
-    fpaths.append(findfile('lumfn',  dryrun=False, prefix='', field=None))
-    fpaths.append(findfile('ddp',    dryrun=False, prefix='', field=None))
-    fpaths.append(findfile('ddp_n8', dryrun=False, prefix='', field=None))
+    fpaths.append(findfile('gold',      dryrun=False, prefix='', field=None))
+    fpaths.append(findfile('kE',        dryrun=False, prefix='', field=None))
+    fpaths.append(findfile('zmax',      dryrun=False, prefix='', field=None))
+    fpaths.append(findfile('vmax',      dryrun=False, prefix='', field=None))
+    fpaths.append(findfile('lumfn',     dryrun=False, prefix='', field=None))
+    fpaths.append(findfile('ddp',       dryrun=False, prefix='', field=None))
+    fpaths.append(findfile('ddp_n8',    dryrun=False, prefix='', field=None))
+
+    for field in fields:
+        for ii, _ in enumerate(d8_limits):
+            fpaths.append(findfile('ddp_n8_d0', dryrun=False, prefix='', field=field, utier=ii))
     
     print('\n\n----  SUPPORTED FPATHS    ----\n')
 
     for fp in fpaths:
-        print('{}\t\t{}'.format(fp.ljust(100), os.path.isfile(fp)))
+        if os.path.isfile(fp):
+            mtime = os.path.getmtime(fp)
+            mtime = datetime.datetime.utcfromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
+            
+        else:
+            mtime = ''
+
+        print('{}\t\t{}\t{}'.format(fp.ljust(100), os.path.isfile(fp), mtime))
 
     gold_paths  = sorted(glob.glob(os.environ['GOLD_DIR']    + '/*.fits'))
     rand_paths  = sorted(glob.glob(os.environ['RANDOMS_DIR'] + '/*.fits'))
@@ -59,6 +74,12 @@ if __name__ == '__main__':
     print('\n\n----  UNSUPPORTED FPATHS    ----\n')
 
     for fp in unsupported:
-        print('{}\t\t{}'.format(fp.ljust(100), os.path.isfile(fp)))
+        if os.path.isfile(fp):
+            mtime = os.path.getmtime(fp)
+            mtime = datetime.datetime.utcfromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            mtime = ''
+
+        print('{}\t\t{}\t{}'.format(fp.ljust(100), os.path.isfile(fp), mtime))
 
     print('\n\nDone.\n\n')

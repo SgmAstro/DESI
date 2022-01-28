5b07662d24ed2460fea1550b64556c0004ad8f4b
sgmAstro <sgmoore@alumni.princeton.edu>
2022-01-28 16:56:11 +0000

    Added supported filenames

--- findfile.py
+++ findfile.py
@@ -28,8 +28,20 @@ def findfile(ftype, dryrun=False, prefix='', field=None, utier='{utier}'):
         parts      = file_types[ftype]
         fpath      = parts['dir'] + '{}_{}{}.fits'.format(parts['id'], parts['ftype'], dryrun)
 
-    else:
-        file_types = {'ddp_n8_d0': {'dir': gold_dir, 'id': 'gama_gold', 'ftype': 'ddp_n8_d0_{}'.format(utier)}}
+    else: 
+        file_types = {'ddp_n8_d0':          {'dir': gold_dir, 'id': 'gama_gold',              'ftype': 'ddp_n8_d0_{}'.format(utier)},\
+                      'ddp_n8_d0_vmax':     {'dir': gold_dir, 'id': 'gama_gold',              'ftype': 'ddp_n8_d0_{}_vmax'.format(utier)},\
+                      'ddp_n8_d0_lumfn':    {'dir': gold_dir, 'id': 'gama_gold',              'ftype': 'ddp_n8_d0_{}_lumfn'.format(utier)},\
+                      'randoms':            {'dir': rand_dir, 'id': 'randoms',                'ftype':'0'},\
+                      'randoms_n8':         {'dir': rand_dir, 'id': 'randoms_N8',             'ftype':'0'},\
+                      'randoms_bd':         {'dir': rand_dir, 'id': 'randoms_bd',             'ftype':'0'},\
+                      'randoms_ddp1':       {'dir': rand_dir, 'id': 'randoms_ddp1',           'ftype':'0'},\
+                      'randoms_ddp1_n8':    {'dir': rand_dir, 'id': 'randoms_ddp1_N8',        'ftype':'0'},\
+                      'randoms_ddp1_bd':    {'dir': rand_dir, 'id': 'randoms_ddp1_bd',        'ftype':'0'},\
+                      'randoms_ddp1_bd_n8': {'dir': rand_dir, 'id': 'randoms_ddp1_bd_ddp_n8', 'ftype':'0'},\
+                      'randoms_bd_ddp_n8':  {'dir': rand_dir, 'id': 'randoms_bd_ddp_n8',      'ftype':'0'}
+                     }
+        
         parts      = file_types[ftype]
 
         fpath      = parts['dir'] + '{}_{}_{}{}.fits'.format(parts['id'], field, parts['ftype'], dryrun)
@@ -50,8 +62,20 @@ if __name__ == '__main__':
     fpaths.append(findfile('ddp_n8',    dryrun=False, prefix='', field=None))
 
     for field in fields:
+        fpaths.append(findfile('randoms',            dryrun=False, prefix='', field=field))
+        fpaths.append(findfile('randoms_n8',         dryrun=False, prefix='', field=field))
+        fpaths.append(findfile('randoms_bd',         dryrun=False, prefix='', field=field))
+        fpaths.append(findfile('randoms_ddp1',       dryrun=False, prefix='', field=field))
+        fpaths.append(findfile('randoms_ddp1_n8',    dryrun=False, prefix='', field=field))
+        fpaths.append(findfile('randoms_ddp1_bd',    dryrun=False, prefix='', field=field))
+        fpaths.append(findfile('randoms_ddp1_bd_n8', dryrun=False, prefix='', field=field))
+        fpaths.append(findfile('randoms_bd_ddp_n8',  dryrun=False, prefix='', field=field))
+
         for ii, _ in enumerate(d8_limits):
-            fpaths.append(findfile('ddp_n8_d0', dryrun=False, prefix='', field=field, utier=ii))
+            fpaths.append(findfile('ddp_n8_d0',       dryrun=False, prefix='', field=field, utier=ii))
+            fpaths.append(findfile('ddp_n8_d0_vmax',  dryrun=False, prefix='', field=field, utier=ii))
+            fpaths.append(findfile('ddp_n8_d0_lumfn', dryrun=False, prefix='', field=field, utier=ii))
+    
     
     print('\n\n----  SUPPORTED FPATHS    ----\n')
 
@@ -72,7 +96,7 @@ if __name__ == '__main__':
     unsupported = [x for x in all_paths if (x not in fpaths and 'dryrun' not in x)]
 
     print('\n\n----  UNSUPPORTED FPATHS    ----\n')
-
+    
     for fp in unsupported:
         if os.path.isfile(fp):
             mtime = os.path.getmtime(fp)
@@ -82,4 +106,7 @@ if __name__ == '__main__':
 
         print('{}\t\t{}\t{}'.format(fp.ljust(100), os.path.isfile(fp), mtime))
 
+    if len(fp) == 0:
+        print('WARNING: NO FILES FOUND!')        
+    
     print('\n\nDone.\n\n')

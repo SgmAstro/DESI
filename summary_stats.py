d46110c8b88812c2a69c2015d0a17c6ebfedddaf
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-02-01 23:21:12 +0000

    Tidied summary stats.

--- summary_stats.py
+++ summary_stats.py
@@ -15,64 +15,62 @@ sys.path.append('{}/DESI'.format(home))
 from   ddp           import tmr_DDP1, tmr_DDP2, tmr_DDP3
 from   delta8_limits import delta8_tier, d8_limits
 
-dat      = Table.read(os.environ['GOLD_DIR'] + 'gama_gold_ddp.fits')
-names    = ['ZMIN', 'ZMAX', 'ZLIMS_NGAL', 'VZ', 'DENS']
 
-tmr_DDPs = np.array([tmr_DDP1, tmr_DDP2, tmr_DDP3])
-tmr_ngold_ratio = np.array([1.002, 0.591, 0.097])
-tmr_ddp_ratio = np.array([1., 0.589, 0.097])
+dat              = Table.read(os.environ['GOLD_DIR'] + '/gama_gold_ddp.fits')
+names            = ['ZMIN', 'ZMAX', 'VZ', 'DENS']
 
+tmr_DDPs         = np.array([tmr_DDP1, tmr_DDP2, tmr_DDP3])
+tmr_ngold_ratios = np.array([1.002, 0.591, 0.097])
+tmr_ddp_ratios   = np.array([1., 0.589, 0.097])
 
 result   = Table()
 rows     = []
 
 print('\n\n')
 
-for ddp in np.arange(1, 4, 1):
-    row = [ddp, tmr_DDPs[ddp-1][0], tmr_DDPs[ddp-1][1]]
+for ddp, tmr_DDP, tmr_ngold_ratio, tmr_ddp_ratio in zip(np.arange(1, 4, 1), tmr_DDPs, tmr_ngold_ratios, tmr_ddp_ratios):
+    row = [ddp, tmr_DDP[0], tmr_DDP[1]]
     
     for col in names:
-        try:
-            row += [dat.meta['DDP{}_{}'.format(ddp, col)]]
-        except:
-            row += [dat.meta['DDP{}{}'.format(ddp, col)]]
-    
-    row += [dat.meta['DDP{}_NGAL'.format(ddp)]/ dat.meta['GOLD_NGAL']]
-    row += [tmr_ngold_ratio[ddp-1]]
+        row += [dat.meta['DDP{}_{}'.format(ddp, col)]]
         
-    row = tuple(row)                
+    row += [dat.meta['DDP{}{}'.format(ddp, 'ZLIMS_NGAL')]]
+    row += [1. * dat.meta['DDP{}_NGAL'.format(ddp)] / dat.meta['GOLD_NGAL']]
+    row += [tmr_ngold_ratio]
+        
+    row = tuple(row)         
     rows.append(row)
 
-    dat.meta['DDP{}_NGAL'.format(ddp)]/ dat.meta['GOLD_NGAL']
-
-# TODO: GOLD_TMR -> REF_TMR.
-names += ['N_NGOLD', 'N_NGOLD_TMR']
-    
-names  = ['DDP', 'MIN_M', 'MAX_M'] + names
+names  = ['DDP', 'MIN_M', 'MAX_M'] + names + ['ZLIMS_NGAL', 'N_NGOLD', 'N_NREF_TMR']
 result = Table(rows=rows, names=names)
 
-result['ZLIMS_NGAL'] = result['ZLIMS_NGAL'] / 10**3
-result['VZ'] = result['VZ'] / 10**6
-result['DENS'] = result['DENS'] / 10**-3
-result['N_GAL/N_GAL_MAX'] = result['ZLIMS_NGAL'] / max(result['ZLIMS_NGAL'])
+result['ZLIMS_NGAL']      = result['ZLIMS_NGAL'] / 10**3
+result['VZ']              = result['VZ'] / 10**6
+result['DENS']            = result['DENS'] / 10**-3
+result['N_GAL/N_GAL_MAX'] = 1. * result['ZLIMS_NGAL'] / max(result['ZLIMS_NGAL'])
 
-for name in names:
-    result[name] = np.round(result[name], 3)
+for name in result.dtype.names:
+    result[name]          = np.round(result[name], 3)
     
+result.rename_column('MIN_M', '$M_{Min.}$')
+result.rename_column('MAX_M', '$M_{Max.}$')
 result.rename_column('ZLIMS_NGAL', '$N_{GAL} / 10^3$')
 result.rename_column('VZ', '$V_{DDP}$ / 10^6$')
 result.rename_column('DENS', '$\\rho_{DDP} / 10^{-3}$')
 result.rename_column('N_NGOLD', '$N/N_{GOLD}$')
-result.rename_column('N_NGOLD_TMR', '$N/N_{GOLD}_{TMR}$')
+result.rename_column('N_NREF_TMR', '$N/N_{REF}_{TMR}$')
+result.rename_column('N_GAL/N_GAL_MAX', '$N_{GAL}/N_{GALMAX}$')
 
 result.pprint()
 
 # https://arxiv.org/pdf/1409.4681.pdf
 ascii.write(result, 'tables/Tab2.tex', Writer=ascii.Latex, latexdict=ascii.latex.latexdicts['AA'], overwrite=True)
 
+exit(0)
+
 rows = []
 
-rand = Table.read('{}/data/GAMA4/randoms/randoms_bd_ddp_n8_G9_0.fits'.format(os.environ['HOME']))
+rand = Table.read('{}/randoms_bd_ddp_n8_G9_0.fits'.format(os.environ['RANDOMS_DIR']))
 
 print('\n\n')
 
@@ -94,6 +92,6 @@ result.pprint()
 # https://arxiv.org/pdf/1409.4681.pdf
 ascii.write(result, 'tables/Tab3.tex', Writer=ascii.Latex, latexdict=ascii.latex.latexdicts['AA'], overwrite=True)
 
-subprocess.run('cd {}/tables; pdflatex compile_tables.tex'.format(os.environ['CODE_ROOT']), shell=True, check=True)
+# subprocess.run('cd {}/tables; pdflatex compile_tables.tex'.format(os.environ['CODE_ROOT']), shell=True, check=True)
 
 print('\n\nDone.\n\n')

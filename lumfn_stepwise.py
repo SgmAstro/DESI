ace05a65cda5eef400bc818801983436bbb135b9
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-02-03 14:25:43 +0000

    Working stepwise.

--- lumfn_stepwise.py
+++ lumfn_stepwise.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 
 from   findfile import findfile
+from   schechter import named_schechter
 
 
 def lum_binner(x, dM=0.1):
@@ -17,8 +18,8 @@ def lum_visible(x, dM=0.1):
 
     result = -x/dM + 1./2.
 
-    result[x >= (dM / 2.)] = 0.0
-    result[x <= (dM / 2.)] = 1.0
+    result[x >=  (dM / 2.)] = 0.0
+    result[x <= -(dM / 2.)] = 1.0
     
     return  result
 
@@ -29,21 +30,21 @@ def lumfn_stepwise_eval(vmax, phi_M, phi, phis, phi_Ms, dM, Mcol='MCOLOR_0P0', M
     Ms    = vmax[Mcol]
     Mmins = vmax[Mmin_col]
 
-    num   = np.count_nonzero(lum_binner(Ms - phi_M))
+    num   = np.count_nonzero(lum_binner(phi_M - Ms))
 
     facs  = lum_visible(phi_M - Mmins) 
     facs[facs > 0.0] /= np.sum(dM * phis[:,None] * lum_visible(phi_Ms[:,None] - Mmins[facs > 0.0]), axis=0)
 
-    facs  = np.sum(facs) 
+    den  = np.sum(facs) 
      
     # dM * phis.
-    return  num / facs
+    return  num / den
     
 def lumfn_stepwise(vmax, Mcol='MCOLOR_0P0', Mmin_col='DDPMALL_0P0_VISZ'):
     dM        = 0.1
 
     phi_Ms    = np.arange(-26., -16., dM) 
-    phi_init  = 1.e-5 * np.ones_like(phi_Ms)
+    phi_init  = 1.e-2 * np.ones_like(phi_Ms)
 
     diff      = 1.e99
     phis      = phi_init 
@@ -51,7 +52,7 @@ def lumfn_stepwise(vmax, Mcol='MCOLOR_0P0', Mmin_col='DDPMALL_0P0_VISZ'):
     iteration = 0
 
     while  (diff > 1.e-6):
-        print('Solving for iteration {} with diff. {}'.format(iteration, diff))
+        print('Solving for iteration {:d} with diff. {:.6e}'.format(iteration, diff))
         
         new_phis = []
     
@@ -64,7 +65,15 @@ def lumfn_stepwise(vmax, Mcol='MCOLOR_0P0', Mmin_col='DDPMALL_0P0_VISZ'):
     
         #  Update previous estimate. 
         phis     = new_phis
-        
+
+        iteration += 1
+
+    phis  = phis / dM 
+
+    nn    = dM * np.sum(phis)
+
+    phis /= nn
+
     return  phi_Ms, phis
         
 
@@ -81,16 +90,11 @@ if __name__ == '__main__':
     print(fpath)
 
     vmax = Table.read(findfile('vmax'))
-    # vmax = vmax[:70000]
 
     vmax.pprint()
 
     phi_Ms, phis = lumfn_stepwise(vmax)
 
-    pl.plot(phi_Ms, np.log10(phis))
-
-    pl.xlim(-16., -26.)
-
-    pl.savefig('stepwise.pdf')
-
+    np.savetxt('stepwise.txt', np.c_[phi_Ms, phis])
+    
     print('\n\nDone.\n\n')

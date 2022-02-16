575f3a9a4e89c19de6c1200dc2b65bf1df250847
michaelJwilson <mjwilson@lbl.gov>
2022-02-16 03:19:50 -0800

    Merge with clustering.

--- desi_gold.py
+++ desi_gold.py
@@ -4,15 +4,16 @@ import numpy as np
 import astropy.units as u
 
 from   astropy.coordinates import SkyCoord
-from   astropy.table import Table, vstack, hstack, unique, join
-from   ros_tools import tile2rosette, calc_rosr
-from   gama_limits import gama_field
-from   cartesian import cartesian, rotate
-from   cosmo           import cosmo, distmod
+from   astropy.table       import Table, vstack, hstack, unique, join
+from   ros_tools           import tile2rosette, calc_rosr
+from   gama_limits         import gama_field
+from   cartesian           import cartesian, rotate
+from   cosmo               import cosmo, distmod
+from   lss                 import fetch_lss
 
 
 def desi_gold():
-    from   desiutil.dust import mwdust_transmission
+    from   desiutil.dust                 import mwdust_transmission
     from   desitarget.sv3.sv3_targetmask import desi_mask, bgs_mask
 
 
@@ -86,7 +87,7 @@ def desi_gold():
     desi_zs = desi_zs[desi_zs['TARGETID'].data >= 0]    
     desi_zs = desi_zs[(desi_zs['SV3_BGS_TARGET'].data & bgs_mask['BGS_BRIGHT']) != 0]
 
-    desi_zs['GOOD_Z']     = (desi_zs['ZWARN'] == 0) & (desi_zs['DELTACHI2'] > 40)
+    desi_zs['GOOD_Z'] = (desi_zs['ZWARN'].data == 0) & (desi_zs['DELTACHI2'].data > 40)
 
     desi_zs['ZDESI'] = desi_zs['Z']
 
@@ -95,6 +96,32 @@ def desi_gold():
     ##  Cut DESI to good redshifts.                                                                                                                                                                
     desi_zs['GAMA_FIELD'] = gama_field(desi_zs['TARGET_RA'].data, desi_zs['TARGET_DEC'].data)
 
+    ##  HACK: PHOTSYS ASSUMED S.                                                                                                                                                                     
+    desi_zs['GMAG_DRED']  = 22.5 - 2.5 * np.log10(desi_zs['FLUX_G'].data / mwdust_transmission(desi_zs['EBV'].data, 'G', 'S', match_legacy_surveys=True))
+    desi_zs['RMAG_DRED']  = 22.5 - 2.5 * np.log10(desi_zs['FLUX_R'].data / mwdust_transmission(desi_zs['EBV'].data, 'R', 'S', match_legacy_surveys=True))
+
+    desi_zs['GMR']        = desi_zs['GMAG_DRED'] - desi_zs['RMAG_DRED']
+    desi_zs['DETMAG']     = desi_zs['RMAG_DRED']
+    
+    desi_zs['IN_GOLD']    = desi_zs['GOOD_Z'].data & (desi_zs['ZDESI'] > 0.039)  & (desi_zs['ZDESI'] < 0.263)
+
+    clustering, full      = fetch_lss(pprint=False, sort=False) 
+
+    clustering_ids        = np.unique(clustering['TARGETID'].data)
+    full_ids              = np.unique(full['TARGETID'].data)
+
+    desi_zs['IN_CLUSTERING'] = np.isin(desi_zs['TARGETID'].data, clustering_ids)
+    desi_zs['IN_FULL']       = np.isin(desi_zs['TARGETID'].data, full_ids)
+
+    clustering_cols = ['WEIGHT_ZFAIL', 'WEIGHT', 'NZ']
+    full_cols       = ['NTILE', 'TILES', 'TILELOCIDS', 'LOCATION_ASSIGNED', 'TILELOCID_ASSIGNED', 'COMP_TILE', 'FRACZ_TILELOCID', 'BITWEIGHTS', 'PROB_OBS']
+                       
+    for cols, cat in zip([clustering_cols, full_cols], [clustering, full]):
+        cols       += ['TARGETID']
+                       
+        desi_zs     = join(desi_zs, cat[cols], keys='TARGETID')
+                       
+    ##  Archive step. 
     archive = Table(desi_zs, copy=True)
 
     xyz     = cartesian(desi_zs['TARGET_RA'].data, desi_zs['TARGET_DEC'].data, desi_zs['ZDESI'].data)
@@ -111,15 +138,6 @@ def desi_gold():
 
     desi_zs['LUMDIST'] = cosmo.luminosity_distance(desi_zs['ZDESI'].data)
     desi_zs['DISTMOD'] = distmod(desi_zs['ZDESI'].data)
-
-    ##  HACK: PHOTSYS ASSUMED S. 
-    desi_zs['GMAG_DRED']  = 22.5 - 2.5 * np.log10(desi_zs['FLUX_G'].data / mwdust_transmission(desi_zs['EBV'].data, 'G', 'S', match_legacy_surveys=True))
-    desi_zs['RMAG_DRED']  = 22.5 - 2.5 * np.log10(desi_zs['FLUX_R'].data / mwdust_transmission(desi_zs['EBV'].data, 'R', 'S', match_legacy_surveys=True))
-    
-    desi_zs['GMR']        = desi_zs['GMAG_DRED'] - desi_zs['RMAG_DRED']
-    desi_zs['DETMAG']     = desi_zs['RMAG_DRED']
-
-    desi_zs['IN_GOLD']    = desi_zs['GOOD_Z'].data & (desi_zs['ZDESI'] > 0.039)  & (desi_zs['ZDESI'] < 0.263)
 
     desi_zs.pprint()
 
@@ -212,7 +230,7 @@ def desi_gold():
 
 
 if __name__ == '__main__':
-    gen_desi()
+    desi_gold()
 
     print('Done.')
 

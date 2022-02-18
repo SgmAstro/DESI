6e38f4b3c351deefa8eeb8d8adc377b68f582163
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-02-18 09:59:40 +0000

    stepwise maxlike.

--- lumfn_stepwise.py
+++ lumfn_stepwise.py
@@ -23,7 +23,7 @@ def lum_visible(x, dM=0.1):
     
     return  result
 
-def lumfn_stepwise_eval(vmax, phi_M, phi, phis, phi_Ms, dM, Mcol='MCOLOR_0P0', Mmin_col='DDPMALL_0P0_VISZ'):
+def lumfn_stepwise_eval(vmax, phi_M, phi, phis, phi_Ms, dM, Mcol='DDPMALL_0P0', Mmin_col='DDPMALL_0P0_VISZ', vmax_stepwise=False):
     '''
     Eqn. 2.12, of Efstathiou, Ellis & Peterson.   
     '''
@@ -35,12 +35,15 @@ def lumfn_stepwise_eval(vmax, phi_M, phi, phis, phi_Ms, dM, Mcol='MCOLOR_0P0', M
     facs  = lum_visible(phi_M - Mmins) 
     facs[facs > 0.0] /= np.sum(dM * phis[:,None] * lum_visible(phi_Ms[:,None] - Mmins[facs > 0.0]), axis=0)
 
-    den  = np.sum(facs) 
+    den   = np.sum(facs) 
+
+    if vmax_stepwise:
+        return  den
      
     # dM * phis.
     return  num / den
     
-def lumfn_stepwise(vmax, Mcol='MCOLOR_0P0', Mmin_col='DDPMALL_0P0_VISZ'):
+def lumfn_stepwise(vmax, Mcol='DDPMALL_0P0', Mmin_col='DDPMALL_0P0_VISZ'):
     dM        = 0.1
 
     phi_Ms    = np.arange(-26., -16., dM) 
@@ -59,14 +62,21 @@ def lumfn_stepwise(vmax, Mcol='MCOLOR_0P0', Mmin_col='DDPMALL_0P0_VISZ'):
         for i, (phi_M, phi) in enumerate(zip(phi_Ms, phis)):
             new_phis.append(lumfn_stepwise_eval(vmax, phi_M, phi, phis, phi_Ms, dM, Mcol=Mcol, Mmin_col=Mmin_col))
 
-        new_phis = np.array(new_phis)
+        new_phis   = np.array(new_phis)
         
-        diff     = np.sum((new_phis - phis)**2.)
-    
+        diff       = np.sum((new_phis - phis)**2.)
+     
         #  Update previous estimate. 
-        phis     = new_phis
+        phis       = new_phis
 
         iteration += 1
+
+    _vmaxs = []
+
+    for i, (phi_M, phi) in enumerate(zip(phi_Ms, phis)):
+        _vmaxs.append(lumfn_stepwise_eval(vmax, phi_M, phi, phis, phi_Ms, dM, Mcol=Mcol, Mmin_col=Mmin_col, vmax_stepwise=True))
+
+    vmaxs_stepwise = np.array(_vmaxs)
 
     phis  = phis / dM 
 
@@ -74,7 +84,7 @@ def lumfn_stepwise(vmax, Mcol='MCOLOR_0P0', Mmin_col='DDPMALL_0P0_VISZ'):
 
     phis /= nn
 
-    return  phi_Ms, phis
+    return  phi_Ms, phis, vmaxs_stepwise
         
 
 if __name__ == '__main__':
@@ -89,12 +99,14 @@ if __name__ == '__main__':
 
     print(fpath)
 
-    vmax = Table.read(findfile('vmax'))
+    fpath = '/cosma5/data/durham/dc-moor2/GAMA4/gama_gold_ddp.fits'
+
+    vmax = Table.read(fpath)
 
     vmax.pprint()
 
-    phi_Ms, phis = lumfn_stepwise(vmax)
+    phi_Ms, phis, vmaxs_stepwise = lumfn_stepwise(vmax)
 
-    np.savetxt('stepwise.txt', np.c_[phi_Ms, phis])
+    np.savetxt('stepwise.txt', np.c_[phi_Ms, phis, vmaxs_stepwise])
     
     print('\n\nDone.\n\n')

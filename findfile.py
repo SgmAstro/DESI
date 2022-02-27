cae5cf898f57c085031a4e8869057770cac66cd2
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-02-27 22:13:50 +0000

    readd assert

--- findfile.py
+++ findfile.py
@@ -130,9 +130,8 @@ def findfile(ftype, dryrun=False, prefix=None, field=None, utier='{utier}', surv
     else:
         raise NotImplementedError()
     
-    # TODO: Re-add assert and test
-    #if field != None:
-    #    assert field in fields
+    if field != None:
+        assert field in fields
     
     if dryrun:
         dryrun = '_dryrun'

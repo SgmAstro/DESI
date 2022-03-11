ab85c381236f4e5da29c097447091073c272b801
sgmAstro <sgmoore@alumni.princeton.edu>
2022-03-11 13:08:32 +0000

    Update config

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
-class config(object):
-    def __init__(self):
-        self.vmax_fillfactor = True
-        self.self_count = True
+class config:
+    def __init__(self, ff=False, self_count=False):
+        self.vmax_fillfactor = ff
+        self.self_count = self_count
         
+    def vmax(self):
+        return self.vmax_fillfactor
         
+    def sc(self):
+        return self.self_count
     
-    
+
+configuration = config(ff=True, self_count=True)
+
+configuration.sc()

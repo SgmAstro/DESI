2aaf04de203570f66fd1557d7c6361558e13f8a5
sgmAstro <sgmoore@alumni.princeton.edu>
2022-03-18 18:28:28 +0000

    Remove arg

--- randoms.py
+++ randoms.py
@@ -14,7 +14,7 @@ from   findfile          import fetch_fields, findfile, overwrite_check, call_si
 from   gama_limits       import gama_limits, gama_field
 from   bitmask           import lumfn_mask, consv_mask
 
-def randoms(field='G9', survey='gama', density=1., zmin=0.039, zmax=0.263, dryrun=False, prefix='', seed=314, oversample=8, realz=0, conservative=False):
+def randoms(field='G9', survey='gama', density=1., zmin=0.039, zmax=0.263, dryrun=False, prefix='', seed=314, oversample=8, realz=0):
     start   = time.time()
 
     fields  = fetch_fields(survey)
@@ -208,7 +208,6 @@ if __name__ == '__main__':
     parser.add_argument('--density',      help='Random density per (Mpc/h)^3', default=1.0, type=float)
     parser.add_argument('--oversample',   help='Oversampling factor for fillfactor counting.', default=8, type=int)
     parser.add_argument('--seed',         help='Random seed.', default=314, type=int)
-    parser.add_argument('--conservative',  help='Use conservative values', action='store_true', default=False)
     
     # Defaults to GAMA Gold limits. 
     parser.add_argument('--zmin', type=np.float32, help='Minimum redshift limit', default=0.039)
@@ -226,11 +225,10 @@ if __name__ == '__main__':
 
     density = args.density
     oversample = args.oversample
-    conservative = args.conservative
 
     assert oversample in np.arange(1, 21, 1)
     
     for xx in [1, oversample]:        
-        randoms(field=field, survey=survey, density=density, zmin=zmin, zmax=zmax, dryrun=dryrun, prefix=prefix, seed=seed, oversample=xx, realz=realz, conservative=conservative)
+        randoms(field=field, survey=survey, density=density, zmin=zmin, zmax=zmax, dryrun=dryrun, prefix=prefix, seed=seed, oversample=xx, realz=realz)
 
 

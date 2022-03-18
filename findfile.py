9479ff3cef4a496dcf25d83019fdb1f58c14f284
sgmAstro <sgmoore@alumni.princeton.edu>
2022-03-18 14:13:34 +0000

    Amend reset/file_check

--- findfile.py
+++ findfile.py
@@ -31,16 +31,12 @@ def reset():
     sys.path.append('{}/DESI/bin'.format(os.environ['HOME']))
     from pipeline import run_command
 
-    home = os.environ['HOME']
-    os.chdir(f'{home}')
-
-    cmds = []
-    cmds.append('rm -rf {}/*'.format(os.environ['RANDOMS_DIR']))
-    cmds.append('rm -rf {}/gama*'.format(os.environ['GOLD_DIR']))
-    cmds.append('rm -rf {}/desi_*'.format(os.environ['GOLD_DIR']))
-
-    for cmd in cmds:    
+    fpaths= file_check(return_success=False, immutable=True)
+    
+    for fpath in fpaths:
+        cmd = f'rm -rf {fpath}'
         out = run_command(cmd, noid=True)
+
 
 def call_signature(dryrun, argv):
     if dryrun:
@@ -287,7 +283,7 @@ def findfile(ftype, dryrun=False, prefix=None, field=None, utier='{utier}', surv
         
     return  fpath
 
-def file_check(dryrun=None):        
+def file_check(dryrun=None, return_success=True, immutable=False):
     try:
         dryrun = os.environ['DRYRUN']
 
@@ -316,38 +312,50 @@ def file_check(dryrun=None):
                 fpaths.append(findfile('ddp_n8_d0',       dryrun=False, field=field, utier=ii, survey=survey))
                 fpaths.append(findfile('ddp_n8_d0_vmax',  dryrun=False, field=field, utier=ii, survey=survey))
                 fpaths.append(findfile('ddp_n8_d0_lumfn', dryrun=False, field=field, utier=ii, survey=survey))
+
+    if return_success == True:
+        print('\n\n----  SUPPORTED FPATHS    ----\n')
+
+        for fp in fpaths:
+            if os.path.isfile(fp):
+                mtime = os.path.getmtime(fp)
+                mtime = datetime.datetime.utcfromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
+
+            else:
+                mtime = ''
+
+            print('{}\t\t{}\t{}'.format(fp.ljust(100), os.path.isfile(fp), mtime))
+
         
-    print('\n\n----  SUPPORTED FPATHS    ----\n')
+    if not immutable:
+        gold_paths  = sorted(glob.glob(os.environ['GOLD_DIR']    + '/*.fits'))
+        rand_paths  = sorted(glob.glob(os.environ['RANDOMS_DIR'] + '/*.fits'))
+        all_paths   = gold_paths + rand_paths
 
-    for fp in fpaths:
-        if os.path.isfile(fp):
-            mtime = os.path.getmtime(fp)
-            mtime = datetime.datetime.utcfromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
-            
-        else:
-            mtime = ''
+    else:
+        gama_gold_paths  = sorted(glob.glob(os.environ['GOLD_DIR']    + '/gama*.fits'))
+        desi_gold_paths  = sorted(glob.glob(os.environ['GOLD_DIR']    + '/desi_*.fits'))
+        rand_paths  = sorted(glob.glob(os.environ['RANDOMS_DIR'] + '/gama*.fits'))
+        all_paths   = gama_gold_paths + desi_gold_paths + rand_paths
+        
+    if return_success == False:
+        return all_paths
 
-        print('{}\t\t{}\t{}'.format(fp.ljust(100), os.path.isfile(fp), mtime))
-
-    gold_paths  = sorted(glob.glob(os.environ['GOLD_DIR']    + '/*.fits'))
-    rand_paths  = sorted(glob.glob(os.environ['RANDOMS_DIR'] + '/*.fits'))
-    all_paths   = gold_paths + rand_paths
-    
     unsupported = [x for x in all_paths if x not in fpaths]
     unsupported = [x for x in unsupported if 'dryrun' not in x]
 
-    print('\n\n----  UNSUPPORTED FPATHS    ----\n')
     
-    for fp in unsupported:
-        if os.path.isfile(fp):
-            mtime = os.path.getmtime(fp)
-            mtime = datetime.datetime.utcfromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
-        else:
-            mtime = ''
+    if return_success == True:
+        print('\n\n----  UNSUPPORTED FPATHS    ----\n')
 
-        print('{}\t\t{}\t{}'.format(fp.ljust(100), os.path.isfile(fp), mtime))
+        for fp in unsupported:
+            if os.path.isfile(fp):
+                mtime = os.path.getmtime(fp)
+                mtime = datetime.datetime.utcfromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
+            else:
+                mtime = ''
 
-    # print('\n\n----  Multiple fields    ----\n')
+            print('{}\t\t{}\t{}'.format(fp.ljust(100), os.path.isfile(fp), mtime))
 
     return  ~np.all([os.path.isfile(fp) for fp in all_paths])
 

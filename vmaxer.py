1c53ba6b04cb90046fb01acd563417b2707a644b
sgmAstro <sgmoore@alumni.princeton.edu>
2022-03-21 17:14:36 +0000

    Remove rand dep. in vmaxer

--- vmaxer.py
+++ vmaxer.py
@@ -3,17 +3,11 @@ import numpy         as     np
 from   astropy.table import Table
 from   cosmo         import volcom
 
-def vmaxer(dat, zmin, zmax, zcol, extra_cols=[], rand=None, conservative=False):
-    
-    # if acceptable, can remove zcol as an argument
-    #assert  dat[zcol].min() <= zmin
-    #assert  dat[zcol].max() >= zmax
+def vmaxer(dat, zmin, zmax, zcol, extra_cols=[], rand=None, fillfactor=True, conservative=False):
 
     assert  dat['ZSURV'].min() <= zmin
     assert  dat['ZSURV'].max() >= zmax
-    
-    zcol = 'ZSURV'
-    
+        
     extra_cols += ['MCOLOR_0P0', 'FIELD', 'WEIGHT_STEPWISE', 'IN_D8LUMFN']
 
     if rand is not None:
@@ -28,8 +22,10 @@ def vmaxer(dat, zmin, zmax, zcol, extra_cols=[], rand=None, conservative=False):
     result      = result[result['ZSURV'] >= zmin]
     result      = result[result['ZSURV'] <= zmax]
     
-    if conservative:
-        result  = result[result['CONSERVATIVE'] == 0] 
+    # I think this is better incorporated in lumfn
+    # ddp.fits does not have CONSERVATIVE
+    #if conservative:
+    #    result  = result[result['CONSERVATIVE'] == 0] 
 
     zmin        = result['ZSURV'].min()
     zmax        = result['ZSURV'].max()
@@ -43,12 +39,17 @@ def vmaxer(dat, zmin, zmax, zcol, extra_cols=[], rand=None, conservative=False):
                         'FORCE_ZMAX': zmax,\
                         'VOLUME':       VV})
 
-    if rand is not None:
+    # TODO: shouldn't introduce a rand dependence here.
+    # I don't think the rand is even used?
+    # if rand is not None:
+    if True:
+        '''
         vmax_rand                 = rand[(zmin < rand['Z']) & (rand['Z'] < zmax)]
 
         if conservative:
             vmax_rand             = vmax_rand[vmax_rand['CONSERVATIVE'] == 0]
-
+        '''
+        
         # TODO:  Check if we need rand_zmin.
         fillfactor_vmax_min       = result['FILLFACTOR_VMAX'][result['ZMAX'] >= zmin].min()
         fillfactor_vmax_max       = result['FILLFACTOR_VMAX'][result['ZMAX'] <= zmax].max()
@@ -61,9 +62,14 @@ def vmaxer(dat, zmin, zmax, zcol, extra_cols=[], rand=None, conservative=False):
     result['VMAX']  = volcom(result['ZMAX'], area)
     result['VMAX'] -= volcom(result['ZMIN'], area)
 
-    result['VZ']    = volcom(result[zcol], area)
+    result['VZ']    = volcom(result['ZSURV'], area)
     result['VZ']   -= volcom(result['ZMIN'], area)
 
     result.meta['CONSERVATIVE'] = conservative
 
+    if fillfactor:
+        result.meta['FILLFACTOR'] = True
+        result['VMAX'] *= result['FILLFACTOR_VMAX']
+        result = result[result['IN_D8LUMFN'] == 0]
+    
     return  result

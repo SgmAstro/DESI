eada8876a3f5145e4e772325f2cc252811c592f3
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-05-12 15:43:42 +0100

    volfracs fix.

--- volfracs.py
+++ volfracs.py
@@ -8,6 +8,12 @@ def volfracs(rand, bitmasks=['IN_D8LUMFN']):
     Calculate volume fractions, typically used to rescale VMAX from solid angle and 
     (DDP1) redshift defined to account for e.g. density restrictions and sphere completeness
     restrictions.
+
+    Note:
+        At the minute, IN_d8LUMFN is restricted to a fillfactor cut.  As the correction to VMAX
+        due to this cut is zmax dependent, we correct the individual galaxy zmaxs for this.  Therefore,                                                                                             
+        The volume correction for the randoms should be restricted to a strict d8 volume, having applied
+        the fillfactor cut to the randoms already.  Otherwise, we double count this effect (approximately).
     '''
     utiers    = np.unique(rand['DDP1_DELTA8_TIER'].data)
     utiers_zp = np.unique(rand['DDP1_DELTA8_TIER_ZEROPOINT'].data)
@@ -19,18 +25,19 @@ def volfracs(rand, bitmasks=['IN_D8LUMFN']):
     ddp1_rand = rand[rand['DDPZLIMS'][:,0] == 1]
 
     print('DDP1 randoms: {:.6f} < z < {:.6f}'.format(ddp1_rand['Z'].min(), ddp1_rand['Z'].max()))
-    
+
+    for bm in bitmasks:
+        isin      = (ddp1_rand[bm].data == 0)
+
+        # At least a cut on sphere completeness.                                                                                                                                                        
+        ddp1_rand = ddp1_rand[isin]
+
+        print(bm, isin)
+
     for ut in range(len(d8_limits)):
         print()
 
-        # TODO/HACK/BUG/MJW  Shouldn't this be ddp1_rand?  Rather than rand.
         in_tier = (ddp1_rand['DDP1_DELTA8_TIER'].data == ut)
-
-        for bm in bitmasks:
-            # At least a cut on sphere completeness.
-            in_tier &= (ddp1_rand[bm].data == 0)
-
-            print(bm, np.mean(in_tier))
 
         # print(ut, d8_limits[ut], np.mean(d8_limits[ut]))
 

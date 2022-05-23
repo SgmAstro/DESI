624f0881e287022be4ba0d1803d784de1c60c68c
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-05-23 17:25:31 +0100

    nproc 12

--- fillfactor.py
+++ fillfactor.py
@@ -277,7 +277,7 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--dryrun', help='Dryrun.', action='store_true')
     parser.add_argument('-s', '--survey', help='Select survey.', default='gama')
     parser.add_argument('--prefix', help='filename prefix', default='randoms')
-    parser.add_argument('--nproc', help='nproc', default=14, type=int)
+    parser.add_argument('--nproc', help='nproc', default=12, type=int)
     parser.add_argument('--realz', help='Realization number', default=0, type=np.int32)
     parser.add_argument('--nooverwrite',  help='Do not overwrite outputs if on disk', action='store_true')
     parser.add_argument('--oversample',   help='Oversampling factor for fillfactor counting.', default=2, type=int)

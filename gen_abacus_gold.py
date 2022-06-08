a7aa02224250519efad818e4925ca77cac30c529
Sam Moore <sgmoore@princeton.edu>
2022-06-08 06:14:14 -0700

    more fixes and nb

--- gen_abacus_gold.py
+++ gen_abacus_gold.py
@@ -20,7 +20,7 @@ def abacus_gold(dryrun=False):
     #path = "/project/projectdirs/desi/cosmosim/FirstGenMocks/AbacusSummit/CutSky/BGS/z0.200/"
     path = "/project/projectdirs/desi/cosmosim/FirstGenMocks/AbacusSummit/CubicBox/BGS/z0.200/AbacusSummit_base_c000_ph000/"
 
-    opath = findfile(ftype='gold', survey='abacus')
+    opath = findfile(ftype='gold', survey='abacus', dryrun=dryrun)
 
     f = h5py.File(path+"BGS_box_ph000.hdf5", "r")
     
@@ -36,21 +36,27 @@ def abacus_gold(dryrun=False):
 
     abacus_gold = Table([pos[:,0], pos[:,1], pos[:,2], abs_mag, g_r, galtype], names=['CARTESIAN_X', 'CARTESIAN_Y', 'CARTESIAN_Z', 'DETMAG', 'GMR', 'GALTYPE'])
     
+    print(len(abacus_gold))
+    
     
     # restrict magnitudes
+    '''
     abacus_gold.meta['RMAX'] = 12.0
     abacus_gold.meta['RLIM'] = 19.8
 
     isin        = (abacus_gold.meta['RMAX'] <= abacus_gold['DETMAG']) & (abacus_gold['DETMAG'] <= abacus_gold.meta['RLIM'])
     abacus_gold = abacus_gold[isin]
     
-    
+    print(len(abacus_gold))
+    '''
+        
     if dryrun:
-        isin = (abacus_gold['CARTESIAN_X'] <= 10) & (abacus_gold['CARTESIAN_X'] >= -10) & (abacus_gold['CARTESIAN_Y'] <= 10) & (abacus_gold['CARTESIAN_Y'] >= -10) & (abacus_gold['CARTESIAN_Z'] <= 10) & (abacus_gold['CARTESIAN_Z'] >= -10)
+        isin = (abacus_gold['CARTESIAN_X'] <= 20) & (abacus_gold['CARTESIAN_X'] >= -20) & (abacus_gold['CARTESIAN_Y'] <= 20) & (abacus_gold['CARTESIAN_Y'] >= -20) &  (abacus_gold['CARTESIAN_Z'] <= 20) & (abacus_gold['CARTESIAN_Z'] >= -20)
+        
+        abacus_gold = abacus_gold[isin]
     
-    # TODO: Add rot?
-    
-    abacus_gold = abacus_gold[isin]
+    print(len(abacus_gold))
+
     
     print('Writing {}.'.format(opath))
     
